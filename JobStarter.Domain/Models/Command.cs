78fcb43f01c6028f637e602cc60878317916dea2
Kamil Zieliński <47740876+kziel445@users.noreply.github.com>
2024-09-30 21:15:52 +0200

    create command.cs model

---
+++ JobStarter.Domain/Models/Command.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobStarter.Domain.Models
+{
+    internal class Command
+    {
+        public string Text { get; set; }
+
+        public Command(string text) 
+        {
+            Text = text;
+        }
+    }
+}
