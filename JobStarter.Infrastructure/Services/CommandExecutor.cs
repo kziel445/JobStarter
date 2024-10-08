1bb69d30d285e3bdd86136f775b64295643e43de
Kamil Zieliński <47740876+kziel445@users.noreply.github.com>
2024-10-08 21:13:52 +0200

    Update CommandExecutor.cs

--- JobStarter.Infrastructure/Services/CommandExecutor.cs
+++ JobStarter.Infrastructure/Services/CommandExecutor.cs
@@ -1,11 +1,14 @@
 ﻿using JobStarter.Application.Interfaces;
+using JobStarter.Domain.Models;
 using Microsoft.Extensions.Logging;
+using System.Diagnostics;
 
 namespace JobStarter.Infrastructure.Services
 {
     public class CommandExecutor : ICommandExecutor
     {
         public readonly ILogger<ICommandExecutor> _logger;
+        private ProcessStartInfo processStartInfo;
 
         public CommandExecutor(ILogger<ICommandExecutor> logger)
         {
@@ -14,11 +17,25 @@ namespace JobStarter.Infrastructure.Services
 
         public void ExecuteCommand(string commandText)
         {
-            System.Diagnostics.Process.Start("CMD.exe",$"/c {commandText}");
-            _logger.LogError("Infrastructure: Not Impemented");
-            // throw new NotImplementedException();
+            processStartInfo = new ProcessStartInfo("cmd", $"/c {commandText}")
+            {
+                RedirectStandardOutput = true,  // Przekierowanie wyjścia
+                UseShellExecute = false,        // Użycie powłoki systemowej
+                CreateNoWindow = true           // Ukrycie okna cmd
+            };
+
+            Process process = new Process
+            {
+                StartInfo = processStartInfo
+            };
+
+            process.Start();
+
+            // Odczytanie wyniku
+            string result = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+
+            _logger.LogInformation($"Infrastructure: {commandText} executed, result: {result}");
         }
-
-
     }
 }
