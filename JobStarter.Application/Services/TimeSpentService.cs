04bac8ce3d72455fced2353b44ab4686215e50d7
Kamil Zieliński <47740876+kziel445@users.noreply.github.com>
2024-10-13 13:50:02 +0200

    Update TimeSpentService.cs

--- JobStarter.Application/Services/TimeSpentService.cs
+++ JobStarter.Application/Services/TimeSpentService.cs
@@ -20,9 +20,28 @@ namespace JobStarter.Application.Services
             _repository = repository;
         }
 
+        public TimeSpent ParseLine(string line)
+        {
+            var parts = line.Split(',');
+            return new TimeSpent
+            {
+                Id = int.Parse(parts[0]),
+                Date = DateOnly.Parse(parts[1]),
+                Spent = TimeSpan.Parse(parts[2]),
+            };
+        }
+
+        public void SaveAll()
+        {
+
+            throw new NotImplementedException();
+        }
         public IEnumerable<TimeSpent> GetAll()
         {
+            _logger.LogInformation("Start all TimeSpent retrive.");
             var model = _repository.GetAll();
+
+            _logger.LogInformation($"Retrived {model.Count()} items.");
 
             return model;
         }
