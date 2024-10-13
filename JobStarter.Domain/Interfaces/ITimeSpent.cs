691593b642e53f25a6b041913c2392751915b342
Kamil Zieliński <47740876+kziel445@users.noreply.github.com>
2024-10-13 13:05:22 +0200

    Create ITimeSpent.cs

---
+++ JobStarter.Domain/Interfaces/ITimeSpent.cs
@@ -0,0 +1,13 @@
+﻿using JobStarter.Domain.Models;
+
+namespace JobStarter.Application.Interfaces
+{
+    public interface ITimeSpent
+    {
+        IEnumerable<TimeSpent> GetSpent();
+        TimeSpent GetById(int id);
+        void Add(TimeSpent timeSpent);
+        void Update(TimeSpent timeSpent);
+        void Delete(int id);
+    }
+}
